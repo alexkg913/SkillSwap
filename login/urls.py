1000eb8467df2135e036c32878fd0f6b98a1da9e
DUMTao <kreyes@mocs.flsouthern.edu>
2026-09-16 22:32:58 -0400

    Created the user register and user forgot password functions to return respective html files. Added them as directories under login/

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.login_page, name="login"),
+    path("user_register/", views.register_page, name="user_register"),
+    path("user_forgot_password/", views.forgot_pwd_page, name="user_forgot_pwd"),
 
 ]
--- login/views.py
+++ login/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login_page(request):
-    return HttpResponse("This should be the first login page user sees")
+    return render(request, "login/login_general.html")
+
+def register_page(request):
+    return render(request, "login/user_register.html")
+
+def forgot_pwd_page(request):
+    return render(request, "login/forgot_pwd.html")
