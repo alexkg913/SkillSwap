83b6cba7c7f8662b1412d1f22e56b7b010205e82
DUMTao <kreyes@mocs.flsouthern.edu>
2026-09-17 20:10:57 -0400

    Commented model, wont work it yet

--- login/models.py
+++ login/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
+#class User(AbstractUser):
     # Generate unique id to reference, pk equal to this
 
     # Email Section
