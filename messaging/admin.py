f01d00568e53b1338f57d873f1e330da708b1bb3
Priscilla Diame <esinam.prisdiame@gmail.com>
2026-09-22 14:09:48 -0400

    Added DirectMessage migration and register in admin

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DirectMessage
+
 
 # Register your models here.
+
+admin.site.register(DirectMessage)
--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# from events.models import Event
+
+class DirectMessage(models.Model):
+    sender = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return f"From {self.sender} to {self.recipient} at {self.timestamp}"
+
+
